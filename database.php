6ae92617eb23b7e83773b8fa137aa45e9b683ed1
khadarsacad <maxamedsacad124@gmail.com>
2026-07-12 21:40:50 +0300

    adding connection

--- database.php
+++ database.php
@@ -7,7 +7,7 @@ $dbname = "water_database";
 $conn = new mysqli($host, $user, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Xiriirkii database-ka waa uu guuldareystay: " . $conn->connect_error);
+    die("" . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4"); 
